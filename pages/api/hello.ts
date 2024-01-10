62f1d53ca790a0f745d4ab72b674ca5905a69ee9
Amber Johnson <amberjohnsonsmile@gmail.com>
2024-01-10 13:25:02 -0700

    Setting up frontend and open ai api call

---
+++ pages/api/hello.ts
@@ -0,0 +1,15 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import OpenAI from 'openai'
+
+const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY })
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const completion = await openai.chat.completions.create({
+    messages: [{ role: 'user', content: req.body.text }],
+    model: 'gpt-3.5-turbo'
+  })
+  res.status(200).json({ result: completion })
+}
---
+++ pages/index.tsx
@@ -0,0 +1,37 @@
+import { ChangeEvent, useCallback, useState } from "react"
+
+export default function Home() {
+
+  const [ input, setInput ] = useState<string>("")
+  const [ output, setOutput ] = useState<string>("")
+  const handleInput = useCallback(
+    (e: ChangeEvent<HTMLInputElement>) => {
+      setInput(e.target.value)
+    }, []
+  )
+
+  const handleOnClick = async () => {
+    setOutput("Thinking about your question")
+
+    const response = await fetch("/api/hello", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json"
+      },
+      body: JSON.stringify({text: input})
+    })
+
+    const data = await response.json()
+    setInput("")
+    setOutput(data.result.choices[0].text)
+  }
+
+  return (
+    <div>
+      <h1>Ask a question!</h1>
+      <input value={input} onChange={handleInput}></input>
+      <button onClick={handleOnClick}>Submit</button>
+      <h2>Response: {output}</h2>
+    </div>
+  )
+}
